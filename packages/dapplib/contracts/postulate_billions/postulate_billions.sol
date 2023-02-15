ceb7eb4fbecf27ddc54fc722b3ddf731c164aecc
elicharlese <elicharles.e@gmail.com>
2023-02-15 19:10:05 +0000

    Adding functions for alerts and hyperledger setup

--- packages/dapplib/contracts/postulate_billions/postulate_billions.sol
+++ packages/dapplib/contracts/postulate_billions/postulate_billions.sol
@@ -1,15 +1,21 @@
-pragma solidity ^0.4.24;
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity ^0.8.2;
 
 contract postulate_billions {
     function custom_SQL_query_blockchain() public {
         // This is a constructor
+        return new post;
     }
-    function statistics() {
-        function accuracy_statistics() public {
+    function statistics() public {
+        function accurracy_statistics(stats) public {
             // Stats
-    }
+            return new post;
+        }
         function backtesting_statistics() public {
             //Stats
+            return new post;
         }
+        return new post;
     }
 }
+
