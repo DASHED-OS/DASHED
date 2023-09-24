c468fa9fb6375b91779daa4213ca93de0c8f49df
elicharlese <elicharles.e@gmail.com>
2023-09-23 19:50:54 -0700

    Beta

--- web3/postulate-billions/strategy/blessing.py
+++
@@ -1,5 +0,0 @@
-# If you can get the code figured out then translate it from C to Python
-# If not then you can always reverse engineer it from the settings
-
-# TODO: #53 Put the settings for the blessing bot here
-
